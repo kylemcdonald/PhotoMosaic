0823f01863468a00b30590dd47fc096f6449fe2f
Kyle McDonald <kyle@kylemcdonald.net>
2017-02-15 15:58:23 -0800

    removed some ofxCv dependency

--- PhotoMosaic/src/Highpass.h
+++ PhotoMosaic/src/Highpass.h
@@ -12,10 +12,26 @@ public:
             ofxCv::copy(src, dst);
             return;
         }
-        ofxCv::convertColor(src, lab, CV_RGB2Lab);
+        
+        // get cv::Mat from src and dst
+        cv::Mat srcMat = ofxCv::toCv(src);
+        cv::Mat dstMat = ofxCv::toCv(dst);
+        
+        // allocate the first time
+        lab.create(srcMat.rows, srcMat.cols, CV_8UC3);
+        lowpass.create(srcMat.rows, srcMat.cols, CV_8UC1);
+        highpass.create(srcMat.rows, srcMat.cols, CV_8UC1);
+        
+        // convert to Lab color and extract lightness
+        cv::cvtColor(srcMat, lab, CV_RGB2Lab);
         cv::split(lab, labChannels);
-        cv::Mat& lightness = labChannels[0];
-        ofxCv::blur(lightness, lowpass, size);
+        const cv::Mat& lightness = labChannels[0];
+        
+        // do lowpass filter
+        size = ((size / 2) * 2) + 1; // foce size to be odd
+        cv::blur(lightness, lowpass, cv::Size(size, size));
+        
+        // use lowpass to produce highpass
         // could convert to 16s instead of 32f for extra speed
         cv::subtract(lightness, lowpass, highpass, cv::noArray(), CV_32F);
         if (contrast != 1) {
@@ -23,8 +39,9 @@ public:
         }
         highpass += 128; // should be diff for other datatypes
         highpass.convertTo(lightness, lightness.type());
+        
         ofxCv::imitate(dst, src);
-        cv::merge(labChannels, ofxCv::toCv(dst));
-        ofxCv::convertColor(dst, dst, CV_Lab2RGB);
+        cv::merge(labChannels, dstMat);
+        cv::cvtColor(dstMat, dstMat, CV_Lab2RGB);
     }
 };
